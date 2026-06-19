fe4ff6b9388f890f2336b55bebe390b60c686468
forestoracle <forest.oracle@gmail.com>
2026-06-19 23:33:30 +0300

    README.md

--- examples/ShowFloat/TM1638LedKey.cpp
+++ examples/ShowFloat/TM1638LedKey.cpp
@@ -1,3 +1,4 @@
+#include "HardwareSerial.h"
 /*
   Библиотека управления модулем TM1638 Led&Key
 
@@ -10,14 +11,13 @@
 *  Конструктор с пользовательским подключением (можно установить пины, к которым подключен модуль)
 *  Выключен режим автоинкремента адреса (см. функцию reset)
 */
-TM1638LedKey::TM1638LedKey(uint8_t data, uint8_t clock, uint8_t strobe)
-{
-    dataPin   = data;
-    clockPin  = clock;
+TM1638LedKey::TM1638LedKey(uint8_t data, uint8_t clock, uint8_t strobe) {
+    dataPin = data;
+    clockPin = clock;
     strobePin = strobe;
     // Настраиваем выводы МК
     init();
-    reset();                             //
+    reset();  //
 }
 
 /*
@@ -28,22 +28,20 @@ TM1638LedKey::TM1638LedKey(uint8_t data, uint8_t clock, uint8_t strobe)
 *    dataPin   = 8;
 *  Выключен режим автоинкремента адреса (см. функцию reset)
 */
-TM1638LedKey::TM1638LedKey(void)
-{
-    dataPin   = 8;                         // типовое подключение модуля
-    clockPin  = 9;                         //  -
-    strobePin = 7;                         //  -
+TM1638LedKey::TM1638LedKey(void) {
+    dataPin = 8;    // типовое подключение модуля
+    clockPin = 9;   //  -
+    strobePin = 7;  //  -
     // Настраиваем выводы МК
-    init();                                // настройка выводов МК
-    reset();                               // настройка модуля TM1638
+    init();   // настройка выводов МК
+    reset();  // настройка модуля TM1638
 }
 
 /*
 *  Инициализация пинов
 *  В начале работы все пины будут настроены на вывод.
 */
-void TM1638LedKey::init(void)
-{
+void TM1638LedKey::init(void) {
     pinMode(strobePin, OUTPUT);
     pinMode(clockPin, OUTPUT);
     pinMode(dataPin, OUTPUT);
@@ -54,17 +52,16 @@ void TM1638LedKey::init(void)
 *  Обратить внимание, что включен режим автоинкремента адреса.
 *  Каждый следующий байт посылается в следующий новый адрес
 */
-void TM1638LedKey::reset(void)
-{
-    setAutoincOn();                                             // на время инициализации включаем автоинкремент адреса
-    digitalWrite(strobePin, LOW);                               // опускаем строб
-    shiftOut(dataPin, clockPin, LSBFIRST, ADR_TM1638_START);    // начальный адрес = 0xC0
+void TM1638LedKey::reset(void) {
+    setAutoincOn();                                           // на время инициализации включаем автоинкремент адреса
+    digitalWrite(strobePin, LOW);                             // опускаем строб
+    shiftOut(dataPin, clockPin, LSBFIRST, ADR_TM1638_START);  // начальный адрес = 0xC0
     for (uint8_t i = 0; i < 16; i++) {
         shiftOut(dataPin, clockPin, LSBFIRST, 0x00);
     }
     digitalWrite(strobePin, HIGH);
-    leds = B00000000;                                           // все светодиоды выключены
-    setAutoincOff();                                            // автоинкремент адреса отключен
+    leds = B00000000;  // все светодиоды выключены
+    setAutoincOff();   // автоинкремент адреса отключен
 }
 
 /*
@@ -73,8 +70,7 @@ void TM1638LedKey::reset(void)
 *    2. Послать команду
 *    3. Установить на стробе высокий уровень сигнала
 */
-void TM1638LedKey::sendCommand(uint8_t command)
-{
+void TM1638LedKey::sendCommand(uint8_t command) {
     digitalWrite(strobePin, LOW);
     shiftOut(dataPin, clockPin, LSBFIRST, command);
     digitalWrite(strobePin, HIGH);
@@ -86,8 +82,7 @@ void TM1638LedKey::sendCommand(uint8_t command)
 *    3. Послать символ
 *    3. Установить на стробе высокий уровень сигнала
 */
-void TM1638LedKey::sendSymbol(uint8_t addr, uint8_t symbol)
-{
+void TM1638LedKey::sendSymbol(uint8_t addr, uint8_t symbol) {
     digitalWrite(strobePin, LOW);
     shiftOut(dataPin, clockPin, LSBFIRST, addr);    // передаем адрес СВ
     shiftOut(dataPin, clockPin, LSBFIRST, symbol);  // передаем состояние СВ
@@ -95,11 +90,43 @@ void TM1638LedKey::sendSymbol(uint8_t addr, uint8_t symbol)
 }
 
 /*
+*  Чтение кнопок.
+*
+*  Например, возвращаемое значение содержит 4 байта. На модуле 8 кнопок, хватает байта.
+*  Но! Модуль может обрабатывать до 24 кнопок.
+*/
+uint32_t TM1638LedKey::buttons(void) {
+    // TODO (spronin#1#):  разобраться с этой переменной. Она избыточна, диодов то всего 8
+    uint32_t keys = 0;  //
+    uint32_t tmp = 0;
+
+    digitalWrite(strobePin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, CMD_TM1638_KEY_SCAN);
+
+    digitalWrite(clockPin, LOW);  // нужно для работы функции shiftIn, т.к. она подразумевает передачу данных по спадающему сигналу clock,
+    // а у модуля он нарастающий. См. документацию к функции shiftIn
+    pinMode(dataPin, INPUT);
+    digitalWrite(dataPin, HIGH);
+
+    for (uint8_t i = 0; i < 4; i++) {
+        tmp = shiftIn(dataPin, clockPin, MSBFIRST);
+        if (tmp > 0) {
+            tmp >>= i;
+        }
+        keys |= (uint32_t)tmp;
+    }
+
+    pinMode(dataPin, OUTPUT);
+    digitalWrite(dataPin, LOW);
+    digitalWrite(strobePin, HIGH);
+    return keys;
+}  // buttons
+
+/*
 *  Установка яркости
 */
-void TM1638LedKey::setBrightness(uint8_t brightness)
-{
-    uint8_t inc = (brightness > 8)? 8: brightness; // если передано число > 8, то дисплей на полную яркость
+void TM1638LedKey::setBrightness(uint8_t brightness) {
+    uint8_t inc = (brightness > 8) ? 8 : brightness;  // если передано число > 8, то дисплей на полную яркость
     sendCommand(CMD_TM1638_DISPLAY_OFF + inc);
     // Интересно, что если написать так sendCommand(CMD_TM1638_DISPLAY_OFF + (brightness > 8)? 8: brightness);
     // то программа занимает на 8 байт больше места, размер переменных не меняется
@@ -110,24 +137,21 @@ void TM1638LedKey::setBrightness(uint8_t brightness)
 *  В этой переменной будем хранить состояние СВ, которое устанавливается в функции setLED.
 *  Биты, соотвествующие СВ, возвращаются функцией getLED
 */
-void TM1638LedKey::setLEDs(void)
-{
-  // TODO: 
+void TM1638LedKey::setLEDs(void) {
+    // TODO:
 }
 
 /*
 *  Включение автоинкремента адреса
 */
-void TM1638LedKey::setAutoincOn(void)
-{
+void TM1638LedKey::setAutoincOn(void) {
     sendCommand(CMD_TM1638_SET_AUTOINC_ON);
 }
 
 /*
 *  Отключение автоинкремента адреса
 */
-void TM1638LedKey::setAutoincOff(void)
-{
+void TM1638LedKey::setAutoincOff(void) {
     sendCommand(CMD_TM1638_SET_AUTOINC_OFF);
 }
 
@@ -138,10 +162,9 @@ void TM1638LedKey::setAutoincOff(void)
 *  Может принимать значения от 1 до 8 (нумерация слева направо как на плате).
 *  Если передано значение больше 8, то возвращается адрес последнего 8-го СД.
 */
-uint8_t TM1638LedKey::getLEDAddress(uint8_t num)
-{
-    uint8_t n = (num > 8)? 8: num;
-    return ADR_TM1638_START + 1 + (n-1) * 2;
+uint8_t TM1638LedKey::getLEDAddress(uint8_t num) {
+    uint8_t n = (num > 8) ? 8 : num;
+    return ADR_TM1638_START + 1 + (n - 1) * 2;
 }
 /*
 *  Возвращает адрес разряда.
@@ -150,9 +173,8 @@ uint8_t TM1638LedKey::getLEDAddress(uint8_t num)
 *  Может принимать значения от 1 до 8 (нумерация слева направо как на плате).
 *  Если передано значение больше 8, то возвращается адрес последнего 8-го разряда.
 */
-uint8_t TM1638LedKey::getGridAddress(uint8_t num)
-{
-    uint8_t n = (num > 8)? 8: num;
+uint8_t TM1638LedKey::getGridAddress(uint8_t num) {
+    uint8_t n = (num > 8) ? 8 : num;
     return ADR_TM1638_START + (n - 1) * 2;
 }
 
@@ -164,16 +186,15 @@ uint8_t TM1638LedKey::getGridAddress(uint8_t num)
 *  Регистрируем состояние светодиода в переменной leds
 *  для последующего чтения
 */
-void TM1638LedKey::setLED(uint8_t num, uint8_t on)
-{
+void TM1638LedKey::setLED(uint8_t num, uint8_t on) {
     uint8_t n = num;
     n = (num > 8) ? 8 : num;
-    n = (num == 0) ? 1: num;
-    on = (on > 1) ? 1: on;
-    if (on) {                                                // регистрируем состояние СВ
-        leds |= (1 << n-1);
+    n = (num == 0) ? 1 : num;
+    on = (on > 1) ? 1 : on;
+    if (on) {  // регистрируем состояние СВ
+        leds |= (1 << n - 1);
     } else {
-        leds &= ~(1 << n-1);
+        leds &= ~(1 << n - 1);
     }
     sendSymbol(getLEDAddress(n), on);
 }
@@ -184,63 +205,35 @@ void TM1638LedKey::setLED(uint8_t num, uint8_t on)
 *  Нумерация слева направо от 1 до 8
 *  Нужно учитывать, что возвращается не реальное состояние СВ, а бит частной переменной leds
 */
-bool TM1638LedKey::getLED(uint8_t num)
-{
+bool TM1638LedKey::getLED(uint8_t num) {
     uint8_t n = num;
     n = (num > DIGITS) ? DIGITS : num;
     n = (num < 1) ? 1 : num;
-    return bool((1 << n-1)  &  leds);
+    return bool((1 << n - 1) & leds);
 }
 
-void TM1638LedKey::setGrid(uint8_t grid, uint8_t val, bool dp = false)
-{
+void TM1638LedKey::setGrid(uint8_t grid, uint8_t val, bool dp) {
     uint8_t gridAddr = getGridAddress(grid);
-    uint8_t symbol   = NUMBER_FONT[val];
+    uint8_t symbol = NUMBER_FONT[val];
     if (dp) {
         symbol |= (1 << 7);
     }
     sendSymbol(gridAddr, symbol);
-} // setGrid
-
-/*
-*  Чтение кнопок.
-*
-*
-*  Например, возвращаемое значение содержит 4 байта. На модуле 8 кнопок, хватает байта.
-*  Но! Модуль может обрабатывать до 24 кнопок.
-*/
-uint32_t TM1638LedKey::getButtons(void)
-{
-    // TODO (spronin#1#):  разобраться с этой переменной. Она избыточна, диодов то всего 8
-    uint32_t keys = 0;  //
-    uint8_t tmp = 0;
-
-    digitalWrite(strobePin, LOW);
-    shiftOut(dataPin, clockPin, LSBFIRST, CMD_TM1638_KEY_SCAN);
-
-    digitalWrite(clockPin, LOW);  // нужно для работы функции shiftIn, т.к. она подразумевает передачу данных по спадающему сигналу clock,
-    // а у модуля он нарастающий. См. документацию к функции shiftIn
-    pinMode(dataPin, INPUT);
-    digitalWrite(dataPin, HIGH);
-    for (uint8_t i = 0; i < 4; i++) {
-        tmp = shiftIn(dataPin, clockPin, MSBFIRST);
-        if (tmp > 0) {
-            tmp >>= i;
-        }
-        keys |= (uint32_t)tmp;
-    }
-    pinMode(dataPin, OUTPUT);
-    digitalWrite(dataPin, LOW);
-    digitalWrite(strobePin, HIGH);
-    return keys;
-} // getButtons
+}  // setGrid
 
 
-void TM1638LedKey::setGauges(uint8_t gauge1, uint8_t gauge2, uint8_t gauge3, uint8_t gauge4, uint8_t gauge5)
-{
-    uint8_t const seg1     = B00000001;
-    uint8_t const seg3     = B01000000;
-    uint8_t const seg5     = B00001000;
+uint32_t TM1638LedKey::getButtons(void) {
+    uint32_t keys0 = buttons();
+    delay(DEBOUNCE_DELAY);
+    uint32_t keys1 = buttons();
+    if (keys0 == keys1) return keys0; else return 0;
+
+}
+
+void TM1638LedKey::setGauges(uint8_t gauge1, uint8_t gauge2, uint8_t gauge3, uint8_t gauge4, uint8_t gauge5) {
+    uint8_t const seg1 = B00000001;
+    uint8_t const seg3 = B01000000;
+    uint8_t const seg5 = B00001000;
 
     uint8_t const seg2Half = B00100000;
     uint8_t const seg2Full = B00100010;
@@ -266,33 +259,30 @@ void TM1638LedKey::setGauges(uint8_t gauge1, uint8_t gauge2, uint8_t gauge3, uin
         if (counter <= gauge2 >> 1) {
             segs |= seg2Full;
         }
-        if ((gauge2 % 2 == 1) && ((counter-1) == (gauge2 >> 1))) {
+        if ((gauge2 % 2 == 1) && ((counter - 1) == (gauge2 >> 1))) {
             segs |= seg2Half;
         }
         // 4-16
         if (counter <= gauge4 >> 1) {
             segs |= seg4Full;
         }
-        if ((gauge4 % 2 == 1) && ((counter-1) == (gauge4 >> 1))) {
+        if ((gauge4 % 2 == 1) && ((counter - 1) == (gauge4 >> 1))) {
             segs |= seg4Half;
         }
         sendSymbol(getGridAddress(counter), segs);
     }
-} // setGauges
+}  // setGauges
 
-void TM1638LedKey::clear(void)
-{
+void TM1638LedKey::clear(void) {
     for (int counter = 1; counter <= 8; counter++) {
-        setGrid(counter, CLEAR, false );
+        setGrid(counter, CLEAR, false);
     }
 }
 
-void TM1638LedKey::showFloat(uint32_t val, uint8_t dp, uint8_t pos)
-{
+void TM1638LedKey::showFloat(uint32_t val, uint8_t dp, uint8_t pos) {
     uint8_t counter, curPos;
     char vals[DIGITS];
     snprintf(vals, 9, "%ld", val);
-    Serial.println(val);
     counter = 0;
     curPos = (pos < 1) ? 1 : pos;
     curPos = (pos > DIGITS) ? DIGITS : pos;
@@ -300,12 +290,11 @@ void TM1638LedKey::showFloat(uint32_t val, uint8_t dp, uint8_t pos)
     while (counter < DIGITS && vals[counter] != '\0' && curPos <= DIGITS) {
         //for (counter = 0; counter < 8; counter++) {
         if (vals[counter] == '-') {
-            setGrid(curPos, MINUS, (counter + 1 == dp) );
+            setGrid(curPos, MINUS, (counter + 1 == dp));
         } else {
-            setGrid(curPos, vals[counter] - 48, (counter + 1 == dp) );
+            setGrid(curPos, vals[counter] - 48, (counter + 1 == dp));
         }
         counter++;
         curPos++;
     }
 }
-
--- examples/test_keys/test_keys.ino
+++ examples/test_keys/test_keys.ino
@@ -10,7 +10,7 @@ uint8_t demoDelay = 50;
 
 TM1638LedKey tm(data, clock, strobe);
 
-void getbuttons(void) {
+void getButtons(void) {
     uint32_t keys = 0;
     uint8_t counter;
     keys = tm.getButtons();
@@ -30,25 +30,15 @@ void getbuttons(void) {
 }  // getButtons
 
 void setup() {
+    uint8_t count;
     // Устанавливаем яркость
     tm.setBrightness(3);
     Serial.begin(9600);
-    tm.setLED(8, 1);
-    delay(LED_DELAY);
-    tm.setLED(7, 1);
-    delay(LED_DELAY);
-    tm.setLED(6, 1);
-    delay(LED_DELAY);
-    tm.setLED(5, 1);
-    delay(LED_DELAY);
-    tm.setLED(4, 1);
-    delay(LED_DELAY);
-    tm.setLED(3, 1);
-    delay(LED_DELAY);
-    tm.setLED(2, 1);
-    delay(LED_DELAY);
-    tm.setLED(1, 1);
-    delay(LED_DELAY);
+    for (count = 1; count <= 8; count++) {
+        tm.setLED(count, 1);
+        delay(LED_DELAY);
+    }
+
     tm.clear();
 }
 //
@@ -57,5 +47,5 @@ void setup() {
 void loop() {
     tm.clear();
     delay(100);
-    getbuttons();
+    getButtons();
 }
